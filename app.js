80a31c462b3473651806b97a481b71e021266641
40228872 <fmckeever04@qub.ac.uk>
2025-01-23 14:03:22 +0000

    Fixed the s

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var employeesRouter = require("./routes/employees");
+var employeeRouter = require("./routes/employees");
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/employees", employeesRouter);
+app.use("/employees", employeeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
